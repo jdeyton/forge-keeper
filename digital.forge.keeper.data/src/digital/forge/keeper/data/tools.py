6a23554458ef50901a969ded36d9c15032400178
Jordan <jordan.deyton@gmail.com>
2019-11-28 22:44:09 -0600

    Adds methods to handle database initialization/destruction.
    Signed-off-by: Jordan <jordan.deyton@gmail.com>

---
+++ digital.forge.keeper.data/src/digital/forge/keeper/data/tools.py
@@ -0,0 +1,25 @@
+from digital.forge.keeper.data.model import Base
+
+
+def destroy_database(engine):
+    '''
+    Deletes the database structure defined in this package.
+
+    Attempt to drop all database tables defined in this package. This will
+    throw an exception if the tables do not exist.
+
+    :param engine: The engine used to connect to the database.
+    '''
+    Base.metadata.drop_all(bind=engine, checkfirst=False)
+
+
+def initialize_database(engine):
+    '''
+    Creates the database structure defined in this package.
+
+    Attempt to create the database tables defined in this package. This will
+    throw an exception if the tables already exist.
+
+    :param engine: The engine used for connect to the database.
+    '''
+    Base.metadata.create_all(bind=engine, checkfirst=False)
