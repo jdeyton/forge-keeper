e3a20277e425738b8120474f98b55478cc880b69
Jordan Deyton <jordan.deyton@gmail.com>
2020-07-24 23:54:39 -0500

    Removes need to import common into openapi.mk.
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

--- tools/make/openapi.mk
+++ tools/make/openapi.mk
@@ -1,4 +1,5 @@
-JAVA ?= java
+PROJECT_ROOT ?= $(shell git rev-parse --show-toplevel)
+include $(PROJECT_ROOT)/tools/make/common.mk
 
 OPENAPI_JAR ?= openapi-generator-cli-5.0.0-beta.jar
 OPENAPI_JAR_URL ?= https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.0.0-beta/$(OPENAPI_JAR)
