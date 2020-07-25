5b17c1fae4e43462de35955c268e91c46bd12e70
Jordan Deyton <jordan.deyton@gmail.com>
2020-07-24 22:56:20 -0500

    Adds common make rules for poetry usage.
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

--- tools/make/common.mk
+++ tools/make/common.mk
@@ -1,8 +1,23 @@
 # Allow these Python defines to be provided in advance.
 
 # System commands:
-CURL ?= /usr/bin/curl
+CURL ?= /bin/curl
 RM ?= /bin/rm -f
+TOUCH ?= /bin/touch
 
 # Python commands:
 POETRY ?= poetry
+
+# ---- Build Python wheels ---- #
+dist: pyproject.toml
+	$(POETRY) build -f wheel
+clean-dist:
+	$(RM) -r dist
+.PHONY: clean-dist
+
+# ---- Initialize Virtual Environment ---- #
+.venv: pyproject.toml
+	poetry install
+	$(TOUCH) $@
+clean-venv:
+	$(RM) -r .venv
