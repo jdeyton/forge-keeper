5b4a3d52c07aa364b065002f5c8255049189d70b
Jordan Deyton <jordan.deyton@gmail.com>
2020-07-27 23:40:07 -0500

    Refactors SQLAlchemy decorator for poetry under app-decorator-sqlalchemy
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

---
+++ src/app-decorator-sqlalchemy/README.md
@@ -0,0 +1,5 @@
+# digital-forge-app-decorator-sqlalchemy
+
+This project provides a simple Flask decorator that adds a database backend
+using Flask-SQLAlchemy. This only enables such a database. The structure and
+content must be provided by the Flask server/business logic.
---
+++ src/app-decorator-sqlalchemy/src/digital/forge/app/decorator/sqlalchemy.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+"""
+This module provides an app decorator that adds SQLAlchemy features to the app.
+"""
+import flask_sqlalchemy
+
+from digital.forge.app.decorator import Decorator
+
+
+class SQLAlchemyDecorator(Decorator):
+    """
+    A decorator for attaching a SQLAlchemy features to an app.
+
+    Flask-SQLAlchemy includes logic around Flask contexts/sessions and allows
+    you to use a declarative SQLAlchemy model. When using this decorator, you
+    can access the database during a request by using the following logic:
+
+    session = flask.current_app.db.session
+    for foo in session.query(SQLAlchemyFoo).all():
+        # do something
+    """
+
+    def __init__(self, db_url):
+        """
+        Create a new SQLAlchemyDecorator.
+
+        :param db_url: The SQLAlchemy database URL used to connect to the
+        database.
+        """
+        if db_url is None:
+            raise Exception('Database URL must not be null')
+        self._db_url = db_url
+
+    def decorate(self, app):
+        """
+        Adds SQLAlchemy features to the flask app.
+        """
+        app.config['SQLALCHEMY_DATABASE_URI'] = self._db_url
+        app.db = flask_sqlalchemy.SQLAlchemy(app)
