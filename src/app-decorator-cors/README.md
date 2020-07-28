b09594f1dd18cd80dd6a6f46bcb3848d4ce997df
Jordan Deyton <jordan.deyton@gmail.com>
2020-07-27 23:34:38 -0500

    Moves CORS decorator to src/app-decorator-cors and refactors for poetry.
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

---
+++ src/app-decorator-cors/README.md
@@ -0,0 +1,4 @@
+# digital-forge-app-decorator-cors
+
+This project provides a simple Flask decorator that enables Cross-Origin
+Resource Sharing (CORS) for use during development of Flask apps.
---
+++ src/app-decorator-cors/src/digital/forge/app/decorator/cors.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""
+This module provides the CORS decorator implementation.
+"""
+import flask_cors
+
+from digital.forge.app.decorator import Decorator
+
+
+class CORSDecorator(Decorator):
+    """
+    A decorator for enabling Cross Origin Resource Sharing (CORS) for an app.
+
+    CORS is useful primarily during development as most browsers by default
+    will reject requests, such as using the Swagger Editor or Swagger UI to
+    make an API request.
+    """
+
+    def decorate(self, app):
+        flask_cors.CORS(app)
