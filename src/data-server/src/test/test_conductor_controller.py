8f707eb2b8cf806309974129d54e0a347a19e383
Jordan Deyton <jordan.deyton@gmail.com>
2020-07-31 17:15:37 -0500

    Adds remaining unit tests for the conductor controller.
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

--- src/data-server/src/test/test_conductor_controller.py
+++ src/data-server/src/test/test_conductor_controller.py
@@ -6,24 +6,17 @@ This module tests the ConductorController controller.
 from __future__ import absolute_import
 
 from datetime import datetime
-from dateutil.tz import tzutc
 import unittest
 from unittest.mock import MagicMock, patch
 
+from dateutil.tz import tzutc
 from flask import json
 
 from digital.forge.data.models.archive import Archive
-from digital.forge.data.models.archive_inputs import ArchiveInputs
 from digital.forge.data.models.drone import Drone
-from digital.forge.data.models.drone_inputs import DroneInputs
-from digital.forge.data.models.error_response import ErrorResponse
-from digital.forge.data.models.base_model_ import Model
-from digital.forge.data.util import deserialize_model
 from digital.forge.data.sql.model import Archive as SQLArchive
 from digital.forge.data.sql.model import Drone as SQLDrone
-from digital.forge.data.sql.model import Event as SQLEvent
 
-from digital.forge.data.server.encoder import JSONEncoder
 from . import BaseTestCase
 
 
@@ -281,19 +274,20 @@ class TestConductorController(BaseTestCase):
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
 
-        mock_db.query.find.return_value = []
+        mock_db.query.return_value.filter.return_value = []
 
         headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
-            '/archive/{archive_uuid}'.format(archive_uuid='archive_uuid_example'),
+            '/archive/{archive_uuid}'.format(
+                archive_uuid='archive_uuid_example'
+            ),
             method='GET',
             headers=headers)
         self.assert404(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    @unittest.skip
     @patch('digital.forge.data.server.conductor_controller.get_db_session')
     def test_get_archives(self, get_mock_db):
         """Test case for get_archives
@@ -302,6 +296,50 @@ class TestConductorController(BaseTestCase):
         """
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
+
+        uuid1 = '96ccac34-d65f-4b64-98af-53abc8a989b1'
+        uuid2 = '96ccac34-d65f-4b64-98af-53abc8a989b2'
+
+        # Fake value to be returned from the "database":
+        mock_archives = [
+            SQLArchive(
+                archive_uuid=uuid1,
+                name='fake_archive1',
+                description='this does not really exist in a database',
+                data_type='imagination',
+                units='brain cells',
+                creation_time='1984-05-01T23:59:08Z',
+            ),
+            SQLArchive(
+                archive_uuid=uuid2,
+                name='fake_archive2',
+                description='this does not really exist in a database',
+                data_type='imagination',
+                units='brain cells',
+                creation_time='1984-05-01T23:59:08Z',
+            ),
+        ]
+        mock_db.query.return_value.all.return_value = mock_archives
+
+        # Expected output from the API:
+        expected = [
+            Archive(
+                uuid=uuid1,
+                name='fake_archive1',
+                description='this does not really exist in a database',
+                data_type='imagination',
+                units='brain cells',
+                creation_time=datetime(1984, 5, 1, 23, 59, 8, tzinfo=tzutc()),
+            ),
+            Archive(
+                uuid=uuid2,
+                name='fake_archive2',
+                description='this does not really exist in a database',
+                data_type='imagination',
+                units='brain cells',
+                creation_time=datetime(1984, 5, 1, 23, 59, 8, tzinfo=tzutc()),
+            ),
+        ]
 
         headers = {
             'Accept': 'application/json',
@@ -313,7 +351,37 @@ class TestConductorController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    @unittest.skip
+        actual = json.loads(response.data)
+        self.assertEqual(2, len(actual))
+        self.assertEqual(expected[0], Archive.from_dict(actual[0]))
+        self.assertEqual(expected[1], Archive.from_dict(actual[1]))
+
+    @patch('digital.forge.data.server.conductor_controller.get_db_session')
+    def test_get_archives_when_empty(self, get_mock_db):
+        """Test case for get_archives
+
+        Show all archives.
+        """
+        mock_db = MagicMock()
+        get_mock_db.return_value = mock_db
+
+        mock_db.query.return_value.all.return_value = []
+
+        expected = []
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/archive',
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        actual = json.loads(response.data)
+        self.assertEqual(expected, actual)
+
     @patch('digital.forge.data.server.conductor_controller.get_db_session')
     def test_get_drone(self, get_mock_db):
         """Test case for get_drone
@@ -323,17 +391,64 @@ class TestConductorController(BaseTestCase):
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
 
+        uuid = '96ccac34-d65f-4b64-98af-53abc8a989b2'
+
+        # Fake value to be returned from the "database"
+        mock_db.query.return_value.filter.return_value = [
+            SQLDrone(
+                drone_uuid=uuid,
+                name='fake_drone',
+                description='this does not really exist in a database',
+                creation_time='1984-05-01T23:59:08Z',
+            )
+        ]
+
+        # This should be the output model.
+        expected = Drone(
+            uuid=uuid,
+            name='fake_drone',
+            description='this does not really exist in a database',
+            creation_time=datetime(1984, 5, 1, 23, 59, 8, tzinfo=tzutc()),
+        )
+
         headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
-            '/drone/{drone_uuid}'.format(drone_uuid='drone_uuid_example'),
+            '/drone/{drone_uuid}'.format(drone_uuid=uuid),
             method='GET',
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    @unittest.skip
+        # The response payload is a JSON string that must be converted.
+        actual = Drone.from_dict(json.loads(response.data))
+        self.assertEqual(expected, actual)
+
+    @patch('digital.forge.data.server.conductor_controller.get_db_session')
+    def test_get_bad_drone(self, get_mock_db):
+        """Test case for get_drone
+
+        Get a drone's info.
+        """
+        mock_db = MagicMock()
+        get_mock_db.return_value = mock_db
+
+        uuid = '96ccac34-d65f-4b64-98af-53abc8a989b2'
+
+        # Fake value to be returned from the "database"
+        mock_db.query.return_value.filter.return_value = []
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/drone/{drone_uuid}'.format(drone_uuid=uuid),
+            method='GET',
+            headers=headers)
+        self.assert404(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
     @patch('digital.forge.data.server.conductor_controller.get_db_session')
     def test_get_drones(self, get_mock_db):
         """Test case for get_drones
@@ -342,6 +457,42 @@ class TestConductorController(BaseTestCase):
         """
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
+
+        uuid1 = '96ccac34-d65f-4b64-98af-53abc8a989b1'
+        uuid2 = '96ccac34-d65f-4b64-98af-53abc8a989b2'
+
+        # Fake value to be returned from the "database":
+        mock_drones = [
+            SQLDrone(
+                drone_uuid=uuid1,
+                name='fake_drone1',
+                description='this does not really exist in a database',
+                creation_time='1984-05-01T23:59:08Z',
+            ),
+            SQLDrone(
+                drone_uuid=uuid2,
+                name='fake_drone2',
+                description='this does not really exist in a database',
+                creation_time='1984-05-01T23:59:08Z',
+            ),
+        ]
+        mock_db.query.return_value.all.return_value = mock_drones
+
+        # Expected output from the API:
+        expected = [
+            Drone(
+                uuid=uuid1,
+                name='fake_drone1',
+                description='this does not really exist in a database',
+                creation_time=datetime(1984, 5, 1, 23, 59, 8, tzinfo=tzutc()),
+            ),
+            Drone(
+                uuid=uuid2,
+                name='fake_drone2',
+                description='this does not really exist in a database',
+                creation_time=datetime(1984, 5, 1, 23, 59, 8, tzinfo=tzutc()),
+            ),
+        ]
 
         headers = {
             'Accept': 'application/json',
@@ -353,7 +504,37 @@ class TestConductorController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    @unittest.skip
+        actual = json.loads(response.data)
+        self.assertEqual(2, len(actual))
+        self.assertEqual(expected[0], Drone.from_dict(actual[0]))
+        self.assertEqual(expected[1], Drone.from_dict(actual[1]))
+
+    @patch('digital.forge.data.server.conductor_controller.get_db_session')
+    def test_get_drones_when_empty(self, get_mock_db):
+        """Test case for get_drones
+
+        Show all drones.
+        """
+        mock_db = MagicMock()
+        get_mock_db.return_value = mock_db
+
+        mock_db.query.return_value.all.return_value = []
+
+        expected = []
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/drone',
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        actual = json.loads(response.data)
+        self.assertEqual(expected, actual)
+
     @patch('digital.forge.data.server.conductor_controller.get_db_session')
     def test_remove_archive(self, get_mock_db):
         """Test case for remove_archive
@@ -363,25 +544,105 @@ class TestConductorController(BaseTestCase):
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
 
+        uuid = '96ccac34-d65f-4b64-98af-53abc8a989b1'
+
+        fake_archive = SQLArchive(
+            archive_uuid=uuid,
+            name='fake_archive',
+            description='this does not really exist in a database',
+            data_type='imagination',
+            units='brain cells',
+            creation_time='1984-05-01T23:59:08Z',
+        )
+        mock_db.query.return_value.filter.return_value = [fake_archive]
+
         headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
-            '/archive/{archive_uuid}'.format(archive_uuid='archive_uuid_example'),
+            '/archive/{archive_uuid}'.format(archive_uuid=uuid),
             method='DELETE',
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    @unittest.skip
+        mock_db.delete.assert_called_once()
+        deleted_values = mock_db.delete.call_args.args
+        self.assertEqual(1, len(deleted_values))
+        self.assertIs(fake_archive, deleted_values[0])
+        mock_db.commit.assert_called_once()
+
+    @patch('digital.forge.data.server.conductor_controller.get_db_session')
+    def test_remove_bad_archive(self, get_mock_db):
+        """Test case for remove_archive
+
+        Remove an archive.
+        """
+        mock_db = MagicMock()
+        get_mock_db.return_value = mock_db
+
+        mock_db.query.return_value.filter.return_value = []
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/archive/{archive_uuid}'.format(
+                archive_uuid='archive_uuid_example'
+            ),
+            method='DELETE',
+            headers=headers)
+        self.assert404(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        mock_db.delete.assert_not_called()
+        mock_db.commit.assert_not_called()
+
     @patch('digital.forge.data.server.conductor_controller.get_db_session')
     def test_remove_drone(self, get_mock_db):
         """Test case for remove_drone
 
-        Remove a drone.
+        Remove an drone.
         """
         mock_db = MagicMock()
         get_mock_db.return_value = mock_db
+
+        uuid = '96ccac34-d65f-4b64-98af-53abc8a989b1'
+
+        fake_drone = SQLDrone(
+            drone_uuid=uuid,
+            name='fake_drone',
+            description='this does not really exist in a database',
+            creation_time='1984-05-01T23:59:08Z',
+        )
+        mock_db.query.return_value.filter.return_value = [fake_drone]
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/drone/{drone_uuid}'.format(drone_uuid=uuid),
+            method='DELETE',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        mock_db.delete.assert_called_once()
+        deleted_values = mock_db.delete.call_args.args
+        self.assertEqual(1, len(deleted_values))
+        self.assertIs(fake_drone, deleted_values[0])
+        mock_db.commit.assert_called_once()
+
+    @patch('digital.forge.data.server.conductor_controller.get_db_session')
+    def test_remove_bad_drone(self, get_mock_db):
+        """Test case for remove_drone
+
+        Remove an drone.
+        """
+        mock_db = MagicMock()
+        get_mock_db.return_value = mock_db
+
+        mock_db.query.return_value.filter.return_value = []
 
         headers = {
             'Accept': 'application/json',
@@ -390,8 +651,11 @@ class TestConductorController(BaseTestCase):
             '/drone/{drone_uuid}'.format(drone_uuid='drone_uuid_example'),
             method='DELETE',
             headers=headers)
-        self.assert200(response,
+        self.assert404(response,
                        'Response body is : ' + response.data.decode('utf-8'))
+
+        mock_db.delete.assert_not_called()
+        mock_db.commit.assert_not_called()
 
 
 if __name__ == '__main__':
