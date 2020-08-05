4689a836500f6af79dc7859015ee220638660052
Jordan Deyton <jordan.deyton@gmail.com>
2020-08-04 23:58:55 -0500

    Adds main implementation for the py-drone CLI.
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

--- src/py-drone/src/digital/forge/drone/__main__.py
+++ src/py-drone/src/digital/forge/drone/__main__.py
@@ -7,7 +7,37 @@ order to send data to the forge.digital Keeper API.
 """
 
 import argparse
+import json
+from pathlib import Path
+from queue import Queue
+import signal
 import sys
+
+from digital.forge.drone.reader import Reader
+from digital.forge.drone.sender import Sender
+
+
+def _load_config(path):
+    """
+    Load a JSON config from file.
+
+    :param path: The path to the config.
+    :type path: pathlib.Path
+    """
+    with path.open() as file:
+        config = json.load(file)
+
+    required_keys = [
+        'port',
+        'rate',
+        'drone',
+        'archives',
+    ]
+    for key in required_keys:
+        if key not in config:
+            raise ValueError('`{}` not specified in config'.format(key))
+
+    return config
 
 
 def main(argv=None):
@@ -18,18 +48,82 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    main_parser = argparse.ArgumentParser()
+    main_parser = argparse.ArgumentParser(
+        description='Collect and report sensor data for the Forge Keeper API.',
+        epilog='''
+EXIT STATUS
 
-    # TODO: Add arguments.
+The program will exit with a status of 0 for a normal exit and non-zero if any
+error occurs.
 
+The program will exit if any of the following conditions are true:
+    * An error is encountered loading the configuration.
+    * The serial port cannot be opened for read.
+    * SIGINT (CTRL+C) is sent to the process.
+    * SIGTERM is sent to the process.
+''',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    main_parser.add_argument(
+        '-c', '--config',
+        default='/etc/py-drone.conf',
+        help='The path to a config file. (default: %(default)s)'
+    )
+
+    namespace = main_parser.parse_args()
+    config_file = Path(namespace.config).absolute()
+
+    # Load the config file, exiting with a helpful error if it's broken.
+    if not config_file.is_file():
+        print('Error reading config file: Not a file.', file=sys.stderr)
+        sys.exit(1)
     try:
-        arguments = main_parser.parse_args()
-    except Exception as err:
-        print("Error reading arguments: " + str(err), file=sys.stderr)
+        config = _load_config(config_file)
+    except ValueError as err:
+        print('Error reading config file: ' + str(err), file=sys.stderr)
+        sys.exit(1)
+    except PermissionError():
+        print('Error reading config file: Permission denied', file=sys.stderr)
         sys.exit(1)
 
-    # TODO: Do something with the arguments.
+    # Set up the readers and writers.
+    data_queue = Queue(maxsize=100)
+    try:
+        reader = Reader(
+            data_queue=data_queue,
+            port=config['port'],
+            rate=config['rate'],
+        )
+        sender = Sender(
+            data_queue=data_queue,
+            drone=config['drone'],
+            archives=config['archives'],
+            )
+    except ValueError as err:
+        print('Error in config: ' + str(err), file=sys.stderr)
+        sys.exit(1)
 
+    # At this point, all should be well. Start collecting data.
+    sender.start()
+    reader.start()
+
+    # For SIGTERM (termination) and SIGINT (CTRL+C), try to gracefully stop the
+    # child threads.
+    def handler(*_args):
+        print('\nReceived stop signal. Stopping processes...', file=sys.stderr)
+        reader.stop(timeout=0)
+        sender.stop(timeout=10)
+        print('Stopped gracefully.', file=sys.stderr)
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+
+    # Wait on the child threads. If the reader terminates early, that's a sign
+    # there was an error trying to connect to the serial port, and we should
+    # close.
+    reader.join()
+    sender.stop(timeout=10)
     sys.exit(0)
 
 
---
+++ src/py-drone/src/test/test_main.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+"""
+This module tests some of the main script functionality.
+"""
+import datetime
+import pathlib
+import queue
+import unittest
+from unittest.mock import patch
+
+from digital.forge.drone.__main__ import _load_config
+
+
+class TestMain(unittest.TestCase):
+    """
+    Unit tests for the program entrypoint.
+    """
+
+    def setUp(self):
+        current_dir = pathlib.Path(__file__).parent.absolute() 
+        self.cfg = current_dir.joinpath('py-drone.json')
+        self.bad_cfg = current_dir.joinpath('py-drone-bad.json')
+
+    def test_load_config(self):
+        """
+        Tests the config file is loaded properly.
+        """
+        expected = {
+            "port": "/dev/ttyS0",
+            "rate": 9600,
+            "drone": "f26ba799-f4a8-4968-b44b-6a29421695c5",
+            "archives": [
+                "6adb613e-9642-4264-af53-cec6d96dfdae",
+                "b5280bf4-786e-4769-f2ce-88e1d24344de"
+            ]
+        }
+        actual = _load_config(self.cfg)
+        
+        self.assertEqual(expected, actual)
+        
+        with self.assertRaises(ValueError):
+            _load_config(self.bad_cfg)
+
+
+if __name__ == "__main__":
+    unittest.main()
