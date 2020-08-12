41ee13ba0ecf7956c14c77181265c40fc920c4e7
Jordan Deyton <jordan.deyton@gmail.com>
2020-08-11 23:44:45 -0500

    Adds example inputs and queries the database.
    Nothing useful is done with the data yet, just want to test the
    connection works.
    
    Signed-off-by: Jordan Deyton <jordan.deyton@gmail.com>

--- src/monitor/src/monitor/__main__.py
+++ src/monitor/src/monitor/__main__.py
@@ -3,9 +3,34 @@
 This module... TODO
 """
 import argparse
+import contextlib
+from datetime import datetime, timedelta
+import getpass
 import sys
 
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+
+from digital.forge.data.sql.model import Archive, Event
+
 from monitor import __version__
+
+
+@contextlib.contextmanager
+def _connection(Session):
+    """
+    A context manager for database connections.
+    """
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except SQLAlchemyError:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 def main(argv=None):
@@ -15,7 +40,33 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     args = _parse_args(argv)
-    
+
+    # Set up a way to connect to the database.
+    db_pass = getpass.getpass(prompt='Password: ')
+    uri = 'postgresql://%s:%s@%s:%s/%s' % \
+        (args.db_user, db_pass, args.db_host, args.db_port, args.db_name)
+    engine = create_engine(uri)
+    Session = sessionmaker(bind=engine)
+
+    # Filter inputs:
+    drone = 'd42cd874-d6a2-4b93-9398-7650a4f81170'
+    data_start = datetime.now() + timedelta(days=-1)
+
+    # Query the relevant Events.
+    # This could be simpler if we establish the 'relationship' in the ORM pkg.
+    with _connection(Session) as db:
+        for record in db.query(Event.event_time, Event.event_value, Archive.units, Archive.name).\
+                join(Archive, Event.archive_uuid == Archive.archive_uuid).\
+                filter(Event.drone_uuid == drone).\
+                filter(Event.event_time >= data_start).\
+                order_by(Event.event_time.asc()):
+            print('Event: {}: {} ({} {})'.format(
+                record.event_time,
+                record.event_value,
+                record.units,
+                record.name
+            ))
+
 
 def _parse_args(argv):
     main_parser = argparse.ArgumentParser(
@@ -25,8 +76,41 @@ def _parse_args(argv):
         '-v', '--version',
         action='version',
         help='Get the version',
-        version='%(prog)s' + __version__
+        version='%(prog)s ' + __version__
     )
+
+    main_parser.add_argument(
+        '--db-host',
+        default='db',
+        help='The database host.',
+        metavar='DB_HOST',
+        type=str,
+    )
+
+    main_parser.add_argument(
+        '--db-port',
+        default='5432',
+        help='The database port.',
+        metavar='DB_PORT',
+        type=int,
+    )
+
+    main_parser.add_argument(
+        '--db-user',
+        default='postgres',
+        help='The database user.',
+        metavar='DB_USER',
+        type=str,
+    )
+
+    main_parser.add_argument(
+        '--db-name',
+        default='postgres',
+        help='The database name.',
+        metavar='DB_NAME',
+        type=str,
+    )
+
     return main_parser.parse_args(argv)
 
 
